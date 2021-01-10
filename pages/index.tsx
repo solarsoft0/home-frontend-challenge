45f82ec856ed618fe6f28619429b7404a98c6d5b
solarsoft0 <okanlawonemmanuel41@gmail.com>
2021-01-10 08:30:22 +0100

    chore: Setup Nextjs with tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <p className={'text-blue-600'}>Hello Next.js 👋</p>
+  </Layout>
+)
+
+export default IndexPage
